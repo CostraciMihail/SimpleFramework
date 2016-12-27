0972617e6161005e1153813a86f8a9544aad8ef2
Costraci Mihail <mihaicostraci3@gmail.com>
2016-12-27 12:53:15 +0200

    First commit.

---
+++ Simple_Framework/Simple_Framework/Simple_Framework.h
@@ -0,0 +1,19 @@
+//
+//  Simple_Framework.h
+//  Simple_Framework
+//
+//  Created by winify on 12/27/16.
+//  Copyright © 2016 C.Mihail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Simple_Framework.
+FOUNDATION_EXPORT double Simple_FrameworkVersionNumber;
+
+//! Project version string for Simple_Framework.
+FOUNDATION_EXPORT const unsigned char Simple_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Simple_Framework/PublicHeader.h>
+
+
